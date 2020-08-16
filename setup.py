6f4796683e114cb7632814c577507c8bafecf88a
SirNapkin1334 <sirnapkin@protonmail.com>
2020-08-16 00:28:12 -0700

    Fix License
    Honestly I never knew about this part of the license so for years I've been committing that last part to all my projects... :/

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
+# MC-Formatting Python Module - convert MC (Section Symbol) Formatting Codes to ANSI Escape Codes
+# Copyright (C) 2020 SirNapkin1334
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# The author can be contacted via:
+#   Email: sirnapkin@protonmail.com
+#   Twitter: @SirNapkin1334
+#   Discord: @SirNapkin1334#7960
+#   Reddit: u/SirNapkin1334
+#   IRC: SirNapkin1334; Registered on Freenode, EFNet, possibly others
+
+# Adapted from https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
+
 import setuptools
 
 with open("README.md", "r") as fh:
